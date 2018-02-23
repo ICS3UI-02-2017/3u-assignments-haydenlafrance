bed57dff83c5d9fa051d258a487bcd53b33d6e3a
lafrh7794 <lafrh7794@HRH-IC0027325.wrdsb.ca>
2018-02-23 10:50:12 -0500

    assign 2

--- src/Assignment2/B1Q5BONUS.java
+++ src/Assignment2/B1Q5BONUS.java
@@ -7,12 +7,13 @@ package Assignment2;
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.RobotSE;
+import becker.robots.Wall;
 
 /**
  *
  * @author lafrh7794
  */
-public class B1Q2 {
+public class B1Q5BONUS {
 
     /**
      * @param args the command line arguments
@@ -22,26 +23,8 @@ public class B1Q2 {
         City kw = new City();
 
         // put a robot in KW
-        RobotSE steven = new RobotSE(kw, 7, 8, Direction.EAST);
-
-        //move him to *(0, 0)
-        do {
-            steven.turnRight();
-        } while (!steven.isFacingNorth());
-        {
-        }
-        do {
-            steven.move();
-        } while (steven.getStreet() != 0);
-        {
-        }
-
-        steven.turnLeft();
-        do {
-
-            steven.move();
-        } while (steven.getAvenue() != 0);
-        {
-        }
+        RobotSE steven = new RobotSE(kw, 0, 0, Direction.EAST);
+       
+        new Wall(kw, 1, 1, Direction.EAST);
     }
 }
