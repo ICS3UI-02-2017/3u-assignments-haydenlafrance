6072c5fab41f60c97762c072730389cfaa68ad7c
lafrh7794 <lafrh7794@HRH-IC0027325.wrdsb.ca>
2018-04-09 11:18:44 -0400

    assign4

---
+++ src/Assignment6/A6Q1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lafrh7794
+ */
+public class A6Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        //scan input
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many studnets are in the class?");
+        int students = input.nextInt();
+        double[] expenses = new double[students];
+        System.out.println("What are thier marks?");
+        int fun = ne
+        
+        
+        
+        
+        
+    }
+}
